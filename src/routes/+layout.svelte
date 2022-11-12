d2642c836815691cd6b1b9fe25900a10b91c5eeb
Geoff Rich <4992896+geoffrich@users.noreply.github.com>
2022-11-11 16:23:46 -0800

    Initial styles

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,48 @@
+<script>
+	import './app.css';
+</script>
+
+<svelte:head>
+	<title>Advent of SvelteKit</title>
+</svelte:head>
+
+<div class="container">
+	<header>
+		<div>Advent of <s class="vue">Vue</s> <span class="svelte">SvelteKit</span> 2022</div>
+	</header>
+
+	<slot />
+
+	<footer>
+		<p>Solutions by <a href="https://geoffrich.net">Geoff Rich</a></p>
+	</footer>
+</div>
+
+<style>
+	header,
+	footer {
+		margin: 0 auto;
+		max-width: var(--size-content-3);
+	}
+
+	header {
+		font-size: var(--font-size-fluid-2);
+		font-weight: var(--font-weight-7);
+	}
+
+	.svelte {
+		color: var(--svelte);
+	}
+
+	.vue {
+		color: var(--vue);
+	}
+
+	.container {
+		display: grid;
+		grid-template-columns: 1fr;
+		grid-template-rows: auto 1fr auto;
+		height: 100%;
+		justify-content: start;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	const MAX_DAY = 0;
+</script>
+
+<main>
+	<p>
+		The <a href="https://adventofvue.com/">Advent of Vue</a> challenges, but in
+		<a href="https://kit.svelte.dev/">SvelteKit</a>.
+	</p>
+	<ul>
+		{#each { length: MAX_DAY + 1 } as _, idx}
+			<li><a href="/day/{idx}">Day {idx}</a></li>
+		{/each}
+	</ul>
+</main>
+
+<style>
+	main {
+		padding: 1rem;
+		max-width: var(--size-content-3);
+		margin: 0 auto;
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+	}
+</style>
