6d2ce708ad9465caf4ed1dae14d9a8a69f2f8d2d
Geoff Rich <4992896+geoffrich@users.noreply.github.com>
2022-12-03 20:38:07 -0800

    Return timezone header

--- src/routes/day/3/+page.server.ts
+++ src/routes/day/3/+page.server.ts
@@ -1,13 +1,13 @@
 import type { PageServerLoad } from './$types';
-import { browser } from '$app/environment';
 
 const TZ_HEADER = 'x-vercel-ip-timezone';
 
 export const load: PageServerLoad = ({ request }) => {
-	const timezone = request.headers.get(TZ_HEADER) ?? 'America/Los_Angeles';
+	const timezone = request.headers.get(TZ_HEADER) ?? 'America/New_York';
 	// get the current date in the timezone of the requester
 	const localDateTime = new Date().toLocaleString('en-US', { timeZone: timezone });
 	return {
-		date: new Date(localDateTime)
+		date: new Date(localDateTime),
+		timezone
 	};
 };
