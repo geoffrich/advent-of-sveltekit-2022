713662cb7eaa194eb610214ae26860dbd9bc66e5
Geoff Rich <4992896+geoffrich@users.noreply.github.com>
2022-12-03 21:13:27 -0800

    Possibly fix christmas date conversion

--- src/routes/day/3/+page.server.ts
+++ src/routes/day/3/+page.server.ts
@@ -1,4 +1,5 @@
 import type { PageServerLoad } from './$types';
+import { zonedTimeToUtc } from 'date-fns-tz';
 
 // https://vercel.com/docs/concepts/edge-network/headers#x-vercel-ip-timezone
 const TZ_HEADER = 'x-vercel-ip-timezone';
@@ -7,7 +8,9 @@ export const load: PageServerLoad = ({ request }) => {
 	const timezone = request.headers.get(TZ_HEADER) ?? 'America/Los_Angeles';
 	// get the current date in the timezone of the requester
 	const localDateTime = new Date().toLocaleString('en-US', { timeZone: timezone });
-	const targetDateTime = new Date(2022, 11, 25).toLocaleString('en-US', { timeZone: timezone });
+	// get Christmas in the local time and convert to UTC
+	const targetDateTime = zonedTimeToUtc('2022-12-25 00:00:00', timezone);
+
 	return {
 		// tried returning dates, but something got lost over the wire
 		date: new Date(localDateTime).getTime(),
