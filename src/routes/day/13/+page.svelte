8e159cf45986784e818f6433ffe740d31cde342c
Geoff Rich <4992896+geoffrich@users.noreply.github.com>
2022-12-26 12:53:36 -0800

    Use <form>

--- src/routes/day/13/+page.svelte
+++ src/routes/day/13/+page.svelte
@@ -3,35 +3,51 @@
 	import ChristmasLights from './ChristmasLights.svelte';
 	import type { PageData } from './$types';
 	import ChristmasOrnament from './ChristmasOrnament.svelte';
+	import { page } from '$app/stores';
+	import { submitReplaceState } from '$lib/util';
 
 	export let data: PageData;
+
+	let form: HTMLFormElement;
+
+	$: alternate = $page.url.searchParams.has('alternate');
+
+	// note: this won't work without JS because there's no submit button
 </script>
 
 <h1>Christmas tree ornaments</h1>
+<form bind:this={form} on:submit={submitReplaceState}>
+	<label
+		><input
+			type="checkbox"
+			name="alternate"
+			checked={alternate}
+			on:change={() => form.requestSubmit()}
+		/> Alternate ornaments</label
+	>
+</form>
 <div class="grid">
 	<div>
-		<h2>With ornaments slot</h2>
+		{#if alternate}
+			<ChristmasTree size={7}>
+				<svelte:fragment slot="lights">
+					<ChristmasLights random={data.rng} />
+					<ChristmasLights random={data.rng} />
+				</svelte:fragment>
 
-		<ChristmasTree size={7}>
-			<svelte:fragment slot="lights">
-				<ChristmasLights random={data.rng} />
-				<ChristmasLights random={data.rng} />
-			</svelte:fragment>
+				<ChristmasOrnament random={data.rng} slot="even" color="green" />
+				<ChristmasOrnament random={data.rng} slot="odd" color="red" />
+			</ChristmasTree>
+		{:else}
+			<ChristmasTree size={7}>
+				<svelte:fragment slot="lights">
+					<ChristmasLights random={data.rng} />
+					<ChristmasLights random={data.rng} />
+				</svelte:fragment>
 
-			<ChristmasOrnament random={data.rng} slot="ornaments" />
-		</ChristmasTree>
-	</div>
-	<div>
-		<h2>With even/odd slot</h2>
-		<ChristmasTree size={7}>
-			<svelte:fragment slot="lights">
-				<ChristmasLights random={data.rng} />
-				<ChristmasLights random={data.rng} />
-			</svelte:fragment>
-
-			<ChristmasOrnament random={data.rng} slot="even" color="green" />
-			<ChristmasOrnament random={data.rng} slot="odd" color="red" />
-		</ChristmasTree>
+				<ChristmasOrnament random={data.rng} slot="ornaments" />
+			</ChristmasTree>
+		{/if}
 	</div>
 </div>
 
